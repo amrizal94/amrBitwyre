eb35b5501a7da6b26043e0f4a67106200859cb7e
Ari Muhammad Rizal <arimrizal94@gmail.com>
2023-07-06 16:47:39 +0700

    add method GET on endpoint /message to verify message use public key and signature

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/ProtonMail/gopenpgp/v2/crypto"
 	"github.com/ProtonMail/gopenpgp/v2/helper"
 	"github.com/gin-gonic/gin"
 	"github.com/segmentio/kafka-go"
@@ -27,67 +28,122 @@ xC7rjhlogQJ2UPRNac7/AQCuJ9t6PmrMNmFVPVLFlaMSqvY8zLiDN1iR4ahYGEKA
 =0Ejb
 -----END PGP PUBLIC KEY BLOCK-----`
 
-// const privkey = `-----BEGIN PGP SIGNATURE-----
-// Version: ProtonMail
+const signature = `-----BEGIN PGP SIGNATURE-----
+Version: ProtonMail
 
-// wnUEARYIACcFAmSk7XQJkGiBAnZQ9E1pFiEE6WTbvGcKm8Qu644ZaIECdlD0
-// TWkAAOeHAQCQH7Fs4/lyjFwOzppHHB0TOfSG50C9GjlJ9WC8hz3v2QD+Ku2B
-// u10OZBeTjiEagBFKlymSx8U/kd7QBamBIeDE5gU=
-// =HNir
-// -----END PGP SIGNATURE-----` // encrypted private key
+wnUEARYIACcFAmSk7XQJkGiBAnZQ9E1pFiEE6WTbvGcKm8Qu644ZaIECdlD0
+TWkAAOeHAQCQH7Fs4/lyjFwOzppHHB0TOfSG50C9GjlJ9WC8hz3v2QD+Ku2B
+u10OZBeTjiEagBFKlymSx8U/kd7QBamBIeDE5gU=
+=HNir
+-----END PGP SIGNATURE-----`
+const (
+	kafkaTopic = "job1"
+	brokers    = "localhost:50234"
+)
 
 func main() {
-
-	// Configure Kafka writer
-	kafkaTopic := "message"
-	kafkaWriter := kafka.NewWriter(kafka.WriterConfig{
-		Brokers: []string{"localhost:50234"},
-		Topic:   kafkaTopic,
-	})
-
-	// Get the message from request body
 	router := gin.Default()
-	router.POST("/messages", func(ctx *gin.Context) {
-		message, err := ctx.GetRawData()
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, errorResponse(err))
-			return
-		}
 
-		// encrypt binary message using public key
-		armor, err := helper.EncryptBinaryMessageArmored(pubkey, message)
-		if err != nil {
-			log.Fatal(err)
-		}
+	router.POST("/messages", sendMessage)
 
-		// Create a Kafka message
-		kafkaMsg := kafka.Message{
-			Value: []byte(armor),
-		}
-
-		// Publish the Kafka message
-		err = kafkaWriter.WriteMessages(context.Background(), kafkaMsg)
-		if err != nil {
-			ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-			return
-		}
-
-		// Close the Kafka writer
-		err = kafkaWriter.Close()
-		if err != nil {
-			ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-			return
-		}
-		msg := "Message published to Redpanda"
-		log.Println(msg)
-		ctx.JSON(http.StatusOK, gin.H{"message": msg})
-
-	})
+	router.GET("/messages", readMessage)
 
 	err := router.Run("0.0.0.0:8080")
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
+}
+
+func sendMessage(ctx *gin.Context) {
+	req, err := ctx.GetRawData()
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	// encrypt binary message using public key
+	armor, err := helper.EncryptBinaryMessageArmored(pubkey, req)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	// Create a Kafka message
+	kafkaMsg := kafka.Message{
+		Value: []byte(armor),
+	}
+
+	// Configure Kafka writer
+	kafkaWriter := kafka.NewWriter(kafka.WriterConfig{
+		Brokers: []string{brokers},
+		Topic:   kafkaTopic,
+	})
+
+	// Publish the Kafka message
+	err = kafkaWriter.WriteMessages(context.Background(), kafkaMsg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	// Close the Kafka writer
+	err = kafkaWriter.Close()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	msg := "Message published to Redpanda"
+	ctx.JSON(http.StatusOK, gin.H{"message": msg})
+}
+
+func readMessage(ctx *gin.Context) {
+	// Configure Kafka reader
+	kafkaReader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{brokers},
+		Topic:   kafkaTopic,
+	})
+
+	// Read the kafka message
+	kafkaMsg, err := kafkaReader.ReadMessage(context.Background())
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	// Create a plain message from the encrypted message value
+	message := crypto.NewPlainMessage(kafkaMsg.Value)
+
+	// Parse the PGP signature from the armored signature string
+	pgpSignature, err := crypto.NewPGPSignatureFromArmored(signature)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	// Parse the public key from the armored public key string
+	publicKeyObj, err := crypto.NewKeyFromArmored(pubkey)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	// Create a key ring containing the signing public key
+	signingKeyRing, err := crypto.NewKeyRing(publicKeyObj)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	// Verify the detached signature using the key ring and current time
+	err = signingKeyRing.VerifyDetached(message, pgpSignature, crypto.GetUnixTime())
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"message": string(kafkaMsg.Value)})
+
 }
 
 func errorResponse(err error) gin.H {
